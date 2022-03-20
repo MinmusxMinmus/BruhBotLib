b04236e270e8105a8ad396c633e62e8e8f9b5c4d
Min <minmusxminmus@hotmail.com>
2022-03-20 22:08:13 +0100

    space

--- src/main/kotlin/Logging.kt
+++ src/main/kotlin/Logging.kt
@@ -13,6 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with "BruhBot".  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package rmi
 
 import org.slf4j.Logger
